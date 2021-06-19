d31b88bd4299c81b5d84a088457d5984e8300c4e
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2021-06-19 13:05:15 -0300

    Criado migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,31 +4,15 @@ import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import ENTITITES from './models';
 import MODULES from './modules';
 import { AuthModule } from './security/auth/auth.module';
 import { JwtAuthGuard } from './security/auth/jwt-auth.guard';
+import * as connectionOptions from './ormconfig';
 
-const getTypeormCofng = (): any => process.env.NODE_ENV == 'production' ? {
-  url: process.env.CLEARDB_DATABASE_URL,
-} : {
-  host:  process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  username:  process.env.DATABASE_USERNAME,
-  password:  process.env.DATABASE_PASSWORD,
-}
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => Object.assign(getTypeormCofng(), {
-        type: 'mysql',
-        timezone: 'Z',
-        entities: ENTITITES,
-        synchronize: false,
-        database: process.env.DATABASE_DATABASE,
-      })
-    }),
+    TypeOrmModule.forRoot(connectionOptions as any),
     AuthModule,
     ...MODULES,
   ],
